b4f59c3d7c6c1570bac8b6f949cffa953cc5aeb2
Shingi <shingirai.chanakira@partner.bmw.co.za>
2021-11-23 16:44:28 +0200

    feat: add models

---
+++ models/models.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type UssdSession struct {
+	gorm.Model
+	Msisdn           string
+	SessionId        string
+	State            SessionStage
+	SessionStartTime time.Time
+	SelectedCountry  string
+	AmountToSend     float32
+}
+
+type MenuText struct {
+	Text       string
+	Language   string
+	MenuNumber int
+}
+
+type ForeignCurrencyRates struct {
+	BaseCurrency string
+	ZarToKwsRate float32
+	ZarToMwkRate float32
+}
+
+type SessionStage string
+
+const (
+	Complete SessionStage = "COMPLETE"
+	Menu1    SessionStage = "MENU_1"
+	Menu2    SessionStage = "MENU_2"
+	Menu3    SessionStage = "MENU_3"
+	Menu4    SessionStage = "MENU_4"
+)
