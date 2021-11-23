ed6fd911b1617070292b5c70713bca5f9206d543
Shingi <shingirai.chanakira@partner.bmw.co.za>
2021-11-23 17:13:49 +0200

    fix: related files must be in the same directory

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+const (
+	dsn = "root:changeit@tcp(127.0.0.1:3306)/ussd_menu?charset=utf8mb4&parseTime=True&loc=Local"
+)
+
+func main() {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("Failed to open database connection")
+	}
+
+	mysql, _ := db.DB()
+
+	defer mysql.Close()
+
+	db.AutoMigrate(&SessionStage)
+	db.AutoMigrate(&UssdSession)
+	db.AutoMigrate(&MenuText)
+	db.AutoMigrate(&ForeignCurrencyRates)
+}
